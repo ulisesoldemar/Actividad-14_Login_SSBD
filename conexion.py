a3eee1336276896c1f066ea86bf4ec5e086f2239
Ulises Oldemar Ortega Gutierrez <ulises.ortega2937@alumnos.udg.mx>
2020-11-16 14:18:30 -0600

    Añadidos métodos para agregar y consultar películas.

--- conexion.py
+++ conexion.py
@@ -48,3 +48,61 @@ def iniciar_sesion(correo, contra):
         return id[0], True
     else:
         return None, False
+
+def insertar_pelicula(pelicula):
+    titulo = pelicula['titulo']
+    fecha_visto = pelicula['fecha_visto']
+    imagen = pelicula["imagen"]
+    director = pelicula['director']
+    anio = pelicula['anio']
+    usuarioId = pelicula['usuarioId']
+
+    insertar = "INSERT INTO pelicula \
+        (titulo, fecha_visto, imagen, director, anio, usuarioId) \
+        VALUES (%s, %s, %s, %s, %s, %s)"
+    
+    cursor.execute(insertar, 
+    (titulo, fecha_visto, imagen, director, anio, usuarioId))
+    bd.commit()
+
+    if cursor.rowcount:
+        return True
+    else:
+        return False
+
+def get_peliculas():
+    query = "SELECT id, titulo, imagen, fecha_visto, director, anio FROM pelicula"
+    cursor.execute(query)
+    peliculas = []
+    for row in cursor.fetchall():
+        pelicula = {
+            'id': row[0],
+            'titulo': row[1],
+            'imagen': row[2],
+            'fecha_visto': row[3],
+            'director': row[4],
+            'anio': row[5]
+        }
+        peliculas.append(pelicula)
+    
+    return peliculas
+
+def get_pelicula(id):
+    query = "SELECT * FROM pelicula WHERE id = %s"
+    cursor.execute(query, (id,))
+    pelicula = {}
+    row = cursor.fetchone()
+    if row:
+        pelicula['id'] = row[0]
+        pelicula['titulo'] = row[1]
+        pelicula['fecha_visto'] = row[2]
+        pelicula['imagen'] = row[3]
+        pelicula['director'] = row[4]
+        pelicula['anio'] = row[5]
+        pelicula['valoracion'] = row[6]
+        pelicula['favorito'] = row[7]
+        pelicula['resenia'] = row[8]
+        pelicula['compartido'] = row[9]
+    
+    return pelicula
+
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, request
 
 from conexion import crear_usuario, iniciar_sesion
+from conexion import insertar_pelicula, get_peliculas, get_pelicula
 
 app = Flask(__name__)
 
@@ -17,6 +18,25 @@ def usuario():
                 return jsonify({"code": "existe"})
         except:
             return jsonify({"code": "error"})
+
+@app.route("/api/v1/peliculas", methods=["GET", "POST"])
+@app.route("/api/v1/peliculas/<int:id>", methods=["GET"])
+def peliculas(id=None):
+    if request.method == "POST" and request.is_json:
+        try:
+            data = request.get_json();
+            print(data)
+            if insertar_pelicula(data):
+                return jsonify({"code": "ok"})
+            else:
+                return jsonify({"code:" "no"})
+        except:
+            return jsonify({"code": "error"})
+    elif request.method == "GET" and id is None:
+        return jsonify(get_peliculas())
+    elif request.method == "GET" and id is not None:
+        return jsonify(get_pelicula(id))
+    
 
 @app.route("/api/v1/sesiones", methods=["POST"])
 def sesion():
