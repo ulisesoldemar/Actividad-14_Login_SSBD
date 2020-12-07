be305a9544dcfc5c9bd04dff19e4d8fdae4a115a
Ulises Oldemar Ortega Gutierrez <ulises.ortega2937@alumnos.udg.mx>
2020-12-06 19:02:28 -0600

    Añadidas pantallas de login y registro

---
+++ src/app/registro/registro.page.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-registro',
+  templateUrl: './registro.page.html',
+  styleUrls: ['./registro.page.scss'],
+})
+export class RegistroPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onRegistrarse() {
+    console.log("registrarse")
+  }
+}
